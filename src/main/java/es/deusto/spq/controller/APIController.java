3b74de42c5e6416a91f1e7cc138c90a5ede760eb
BasterHouse <unai.basterretxea@opendeusto.es>
2024-04-03 18:40:40 +0200

    Avance APIController

--- src/main/java/es/deusto/spq/controller/APIController.java
+++ src/main/java/es/deusto/spq/controller/APIController.java
@@ -1,8 +1,18 @@
 package es.deusto.spq.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class APIController {
+    private JuegoService juegoService;
 
+    @GetMapping("/crearjuego")
+    public void crearJuego() {
+        juegoService.crearJuego();
+    }
+    
+    
 }
